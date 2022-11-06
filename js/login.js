89558e1ea7b250da7f9607e82a998d5a9fe2235b
Enzo Matias <83880678+E-Matias@users.noreply.github.com>
2022-11-06 17:50:02 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,22 +1,30 @@
-//função de singup
-let nomeUsuario = getElementById('nameSU').value
-let emailUsuario = getElementById('emailSU').value
-let senhaUsuario = getElementById('passwordSU').value
+emailBD = ['enzo.matias@germinare.org.br', 'bernardo.garces@germinare.org.br', 'ana.ornelas@germinare.org.br', 'lucas.baciega@germinare.org.br']
+passwordBD = ['bot$786', '!pinguim', 'Organe!as', '18-10-2004']
 
+let msgErro = document.querySelector('#msgErro')
+
+//O singup será ilustrativo
 function singup() {
         location.href = "login.html"
 }
 
-//função de login
 function login() {
-    let login = document.getElementById('emailf')
-    let senha = document.getElementById('passwordf')
-
-        if(login.value == 'adm@gmail.com' && senha.value == 'senha123') {
-                location.href = "../index.html"
-        }
-        else {
-                alert('Email ou senha incorretos, tente novamente!')
-        }
+    let login = document.getElementById('emailf').value
+    let senha = document.getElementById('passwordf').value
+    if (emailBD.includes(login) == true && passwordBD.includes(senha) == true) {
+        location.href = "../../index.html"
+        console.log('O usuario foi encaminhado para outra página')
+    } else {
+        msgErro.style.display = 'block'
+        console.log('O email ou a senha estão incorretos')
 }
-
+        /*if(login.value == 'adm@gmail.com' && senha.value == 'senha123') {
+                location.href = "../../index.html"
+        } else if (login.value == 'adm@gmail.com' && senha.value != 'senha123') {
+                alert('Senha inválida, tente novamente!')
+        } else if (login.value != 'adm@gmail.com' && senha.value == 'senha123') {
+                alert('Email inválido, tente novamente!')
+        } else {
+                alert('Email e senha inválidos, tente novamente!')
+        }*/
+}
--- js/newsletter.js
+++ js/newsletter.js
@@ -1,8 +1,22 @@
 function changeText() {
-    let textoBtn = document.getElementById('CTB')
-    if (textoBtn.value == 'Inscreva-se') {
-        textoBtn.value = 'Inscrito'
-    } else {
-        textoBtn.value = 'Inscreva-se'
+    //Futuramente pretendo enviar essa Array para um banco de dados onde serão armazenados os nomes dos participantes
+    let inputNewsletter = document.querySelector('#newsletterInput').value
+    let msgNewsletter = document.querySelector('#msgNewsletter')
+    let btn = document.querySelector('#CTB')
+
+    bdEmail = ['enzofreitasmatias@gmail.com', 'enzo.matias@germinare.org.br']
+
+    if (bdEmail.includes(inputNewsletter) == false) {
+        bdEmail.push(inputNewsletter)
+        msgNewsletter.innerHTML = 'Seu email foi cadastrado'
+        msgNewsletter.style.color = '#008000'
+        btn.value = 'Inscrito'
+        console.log(bdEmail)
+    } else  if (btn.value == 'Inscrito'){
+        console.log('Já foi cadastrado o e-mail')
+        msgNewsletter.innerHTML = ''
+    } else if (bdEmail.includes(inputNewsletter) == true) {
+        msgNewsletter.innerHTML = 'Esse e-mail já está sendo usado, use outro email'
+        msgNewsletter.style.color = '#FF0000'
     }
 }
